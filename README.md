c618ed43c713cf496a7ae30f9566738c539f4ba8
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-04-16 22:50:10 -0700

    final example product

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# heroku-nodejs-express-helloworld
+# heroku-memcachier-example
 
-1) heroku login
-2) heroku create
-3) git push heroku master
+Initial Heroku app config
+
+1. heroku login
+2. heroku create
+3. git push heroku master
+
+To add the service to the heroku app
+
+```heroku addons:create memcachier:dev```
+
+to see the config variables (memcachier server, username, password)
+
+```heroku config```
+
+Copy the env variables to local .env file to set up local environment then refresh bash session and re-run the app.
--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4006
+
+const memjs = require('memjs')
+const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+	failover: true,  // default: false
+	timeout: 1,      // default: 0.5 (seconds)
+	keepAlive: true  // default: false
+})
 
 const app = express()
-// app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// some expensive calculation
+const calculatePrime = (n) => {
+	let prime = 1;
+	for (let i = n; i > 1; i--) {
+		let is_prime = true;
+		for (let j = 2; j < i; j++) {
+			if (i % j == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime) {
+			prime = i;
+			break;
+		}
+	}
+	return prime;
+}
+
+const getPrime = n => new Promise((resolve, reject) => {
+	const primeKey = 'primeNumber' + n
+	mc.get(primeKey, (err, val) => {
+		if(err == null && val != null) {
+			resolve(parseInt(val))
+		}
+		else {
+			const primeNumber = calculatePrime(n)
+			mc.set(primeKey, '' + primeNumber, { expires: 10 }, (err, val) => {
+				if (err) console.log(err)
+				resolve(primeNumber)
+			})
+		}
+	})
+})
+
+app.get('/', (req, res) => {
+	const start = new Date()
+	const n = req.query.n || 2000000000
+	getPrime(n).then((primeNumber) => {
+		const took = new Date() - start
+		res.send(`Largest prime smaller than ${n} is ${primeNumber}. Took ${took}ms to calculate.`)
+	})
+})
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
